6e7bd6134cc4eaa927201bfb0b26fc2e6640bc79
Crozet Sébastien <developer@crozet.re>
2020-11-24 12:25:52 +0100

    Modify our build script in order to keep the typescript files inside of the NPM package.
    Fix #3

--- rapier2d/build_typescript.sh
+++ rapier2d/build_typescript.sh
@@ -4,6 +4,6 @@ cp -r ../src.ts/* pkg/.
 echo 'export * from "./rapier_wasm2d"' >pkg/raw.ts
 find pkg/ -type f -print0 | xargs -0 sed -i '/^ *\/\/ #if DIM3/,/^ *\/\/ #endif/{/^ *\/\/ #if DIM3/!{/^ *\/\/ #endif/!d}}'
 tsc
-find pkg/ -type f -name *.ts ! -name *.d.ts -print0 | xargs -0 rm
+# NOTE: we keep the typescripts files into the NPM package for source mapping: see #3
 sed -i 's/"module": "rapier_wasm2d.js"/"module": "rapier.js"/g' pkg/package.json
 sed -i 's/"types": "rapier_wasm2d.d.ts"/"types": "rapier.d.ts"/g' pkg/package.json
--- rapier3d/build_typescript.sh
+++ rapier3d/build_typescript.sh
@@ -4,6 +4,6 @@ cp -r ../src.ts/* pkg/.
 echo 'export * from "./rapier_wasm3d"' >pkg/raw.ts
 find pkg/ -type f -print0 | xargs -0 sed -i '/^ *\/\/ #if DIM2/,/^ *\/\/ #endif/{/^ *\/\/ #if DIM2/!{/^ *\/\/ #endif/!d}}'
 tsc
-find pkg/ -type f -name *.ts ! -name *.d.ts -print0 | xargs -0 rm
+# NOTE: we keep the typescripts files into the NPM package for source mapping: see #3
 sed -i 's/"module": "rapier_wasm3d.js"/"module": "rapier.js"/g' pkg/package.json
 sed -i 's/"types": "rapier_wasm3d.d.ts"/"types": "rapier.d.ts"/g' pkg/package.json
