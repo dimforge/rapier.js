280e6dec624618e7ec59ca1620ad1a3e3fd95870
Crozet Sébastien <developer@crozet.re>
2021-05-23 13:14:20 +0200

    Adjust build scripts to work of MacOS + start adding a CI.

--- rapier3d/build_rust.sh
+++ rapier3d/build_rust.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
 wasm-pack build
-sed -i 's#dimforge_rapier#@dimforge/rapier#g' pkg/package.json
-sed -i 's/"rapier_wasm3d_bg.wasm"/"*"/g' pkg/package.json
+sed -i.bak 's#dimforge_rapier#@dimforge/rapier#g' pkg/package.json
+sed -i.bak 's/"rapier_wasm3d_bg.wasm"/"*"/g' pkg/package.json
+rm pkg/*.bak
