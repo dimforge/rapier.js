ac59bffa2b58a21121ace5abe618087d167fb712
Talin <viridia@gmail.com>
2021-08-22 15:30:16 -0700

    Remove the need for Buffer polyfill.

--- rapier-compat/src3d/init.ts
+++ rapier-compat/src3d/init.ts
@@ -7,5 +7,9 @@ import wasmInit from "../pkg3d/rapier_wasm3d";
  * Has to be called and awaited before using any library methods.
  */
 export async function init() {
-  await wasmInit(Buffer.from(wasmBase64 as unknown as string, "base64"));
+  await wasmInit(
+    Uint8Array.from(globalThis.atob(wasmBase64 as unknown as string), (c) =>
+      c.charCodeAt(0)
+    ).buffer
+  );
 }
