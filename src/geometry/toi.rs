4753f770521a5dc7bbd2d688b4a4a898933890ae
J.C <1571995@qq.com>
2022-02-25 15:26:35 +0800

    added collider.castShape & collider.castCollider

--- src/geometry/toi.rs
+++ src/geometry/toi.rs
@@ -3,6 +3,34 @@ use rapier::geometry::{ColliderHandle, TOI};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+pub struct RawShapeTOI {
+    pub(crate) toi: TOI,
+}
+
+#[wasm_bindgen]
+impl RawShapeTOI {
+    pub fn toi(&self) -> f32 {
+        self.toi.toi
+    }
+
+    pub fn witness1(&self) -> RawVector {
+        self.toi.witness1.coords.into()
+    }
+
+    pub fn witness2(&self) -> RawVector {
+        self.toi.witness2.coords.into()
+    }
+
+    pub fn normal1(&self) -> RawVector {
+        self.toi.normal1.into_inner().into()
+    }
+
+    pub fn normal2(&self) -> RawVector {
+        self.toi.normal2.into_inner().into()
+    }
+}
+
+#[wasm_bindgen]
 pub struct RawShapeColliderTOI {
     pub(crate) handle: ColliderHandle,
     pub(crate) toi: TOI,
