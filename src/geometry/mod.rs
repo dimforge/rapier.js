feedb4550f0c77c9ef6c8912ccdf009aa1d32f96
jcyuan <jcyuan.space@gmail.com>
2022-03-21 15:58:36 +0800

    format

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -3,22 +3,22 @@
 pub use self::broad_phase::*;
 pub use self::collider::*;
 pub use self::collider_set::*;
+pub use self::contact::*;
 pub use self::narrow_phase::*;
 pub use self::point::*;
 pub use self::ray::*;
 pub use self::shape::*;
 pub use self::toi::*;
-pub use self::contact::*;
 
 mod broad_phase;
 mod collider;
 mod collider_set;
+mod contact;
 mod narrow_phase;
 mod point;
 mod ray;
 mod shape;
 mod toi;
-mod contact;
 
 use rapier::dynamics::CoefficientCombineRule;
 use rapier::geometry::InteractionGroups;
