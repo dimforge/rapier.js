0e8db1141afc60578d1f5c8d7abe363667e9d22e
Hendrik Mans <hendrik@mans.de>
2022-05-28 18:53:43 +0200

    Migrate build scripts to yarn tasks

--- rapier2d/build_all.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-rm -rf pkg
-./build_rust.sh
-./build_typescript.sh
-./build_doc.sh
-
-# Copy our package.json over into the build directory
-cp ./package.json ./pkg/
--- rapier2d/build_rust.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-wasm-pack build
-
-# TODO: solve this via .npmignore or `files` in package.json
-# rm pkg/*.bak
-# rm pkg/.gitignore
