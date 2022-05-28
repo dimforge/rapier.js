3b518e76e3e14a4ed69846221821806c485540a5
Hendrik Mans <hendrik@mans.de>
2022-05-28 18:49:48 +0200

    Cleanup

--- rapier2d/build_rust.sh
+++ rapier2d/build_rust.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 wasm-pack build
-sed -i.bak 's#dimforge_rapier#@dimforge/rapier#g' pkg/package.json
-sed -i.bak 's/"rapier_wasm2d_bg.wasm"/"*"/g' pkg/package.json
-rm pkg/*.bak
-rm pkg/.gitignore
+
+# TODO: solve this via .npmignore or `files` in package.json
+# rm pkg/*.bak
+# rm pkg/.gitignore
