0740686f8fe4b7f02119ca1a7c673b81cbc4f22c
Thierry Berger <contact@thierryberger.com>
2024-07-22 17:35:35 +0200

    add more files to side effects

--- rapier3d/build_rust.sh
+++ rapier3d/build_rust.sh
@@ -3,5 +3,10 @@
 npx wasm-pack build
 sed -i.bak 's#dimforge_rapier#@dimforge/rapier#g' pkg/package.json
 sed -i.bak 's/"rapier_wasm3d_bg.wasm"/"*"/g' pkg/package.json
+(
+    cd pkg
+    npm pkg delete sideEffects
+    npm pkg set 'sideEffects[0]'="./*.js"
+)
 rm pkg/*.bak
 rm pkg/.gitignore
