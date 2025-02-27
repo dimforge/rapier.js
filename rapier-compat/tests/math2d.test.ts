91003a6d02d9f949b2ad8f6788ba03b41aad03a4
Thierry Berger <contact@thierryberger.com>
2025-02-27 11:01:21 +0100

    fix tests

--- rapier-compat/tests/math2d.test.ts
+++ rapier-compat/tests/math2d.test.ts
@@ -1,4 +1,4 @@
-import {Vector2, VectorOps} from "../builds/rapier2d-deterministic/pkg";
+import {Vector2, VectorOps} from "../builds/2d-deterministic/pkg";
 
 describe("2d/math", () => {
     test("Vector2", () => {
