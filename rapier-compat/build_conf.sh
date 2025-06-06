7ef5372173a5d758b0e3e5895290ed199a09b0ce
Thierry Berger <contact@thierryberger.com>
2025-06-06 09:24:31 +0200

    use npm for ci to run build_conf ; so wasmpack should be installed :crossed_fingers:

--- rapier-compat/build_conf.sh
+++ rapier-compat/build_conf.sh
@@ -14,7 +14,7 @@ fi
 for config_file_name in "$@"
 do
 
-  ./build-rust.sh $config_file_name
+  ./build_rust.sh $config_file_name
   ./gen_src.sh $config_file_name
   npm run rollup --  --config rollup.config.js --bundleConfigAsCjs --environment BUILD_CONFIG_NAME:${config_file_name}
 
